99a0fd2c31e42900d64a8a47df9776f358181415
i_monibKS <singhamonib1327@gmail.com>
2024-09-03 15:55:05 +0530

    company-controller logic and updated company routes

--- backend/controllers/company-controller.js
+++ backend/controllers/company-controller.js
@@ -1,96 +1,99 @@
+const Company = require("../models/company-model");
 
-const Company = require('../models/company-model') 
+const registerCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
 
-const registerCompany = async (req, res) =>{
-    try {
-        const {companyName} = req.body;
-        if(!companyName) {
-            return res.status(400).json({
-                message: 'Company name is required.',
-                success: false,
-            })
-
-            let company = await Company.findOne({name: companyName});
-            if(company){
-                return res.status(400).json({
-                    message: 'You cannot register same company.',
-                    success: false,
-                })
-            }
-            company = await Company.create({
-                name: companyName,
-                userId: req._id,
-            })
-
-            return res.status(201).json({
-                message: 'Company registered successfully.',
-                company,
-                success: true,
-            })
-        }
-    } catch(err) {
-        console.log(err);
+    if (!companyName) {
+      return res.status(400).json({
+        message: "Company name is required.",
+        success: false,
+      });
     }
-}
+
+    let company = await Company.findOne({ name: companyName });
+    if (company) {
+      return res.status(400).json({
+        message: "You cannot register same company.",
+        success: false,
+      });
+    }
+    company = await Company.create({
+      name: companyName,
+      userId: req.id,
+    });
+
+    return res.status(201).json({
+      message: "Company registered successfully.",
+      company,
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getCompany = async (req, res) => {
-    try {
-        const userId = req.id;  //loggred in userid
-        const companies = await Company.find({userId});
-        if(!companies) {
-            return res.status(404).json({
-                message: 'Companies not found.',
-                success: false,
-            })
-        }
-    } catch(err){
-        console.log(err);
+  try {
+    const userId = req.id; //logged in userid
+    const companies = await Company.find({ userId });
+    if (!companies) {
+      return res.status(404).json({
+        message: "Companies not found.",
+        success: false,
+      });
     }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getCompanyById = async (req, res) => {
-    try{
-        const companyId = req.params.id;
-        const company = await Company.findById(companyId);
-        if(!company) {
-            return res.status(404).json({
-                message: 'Company not found.',
-                success: false,
-            })
-        }
-        return res.status(200).json({
-            company,
-            success: true,
-        })
-    } catch(err) {
-        console.log(err);
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found.",
+        success: false,
+      });
     }
-}
+    return res.status(200).json({
+      company,
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const updateCompany = async (req, res) => {
-    try {
-        const {name, description, website, location} = req.body;
-        const file = req.file;
+  try {
+    const { name, description, website, location } = req.body;
+    const file = req.file;
 
-        //cloudinary .....
+    //cloudinary .....
 
-        const updateData = {name, description, website, location}
+    const updateData = { name, description, website, location };
 
-        const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true});
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+    });
 
-        if(!company) {
-            return res.status(404).json({
-                message: 'Company not found.',
-                success: false,
-            })
-        }
-        return res.status(200).json({
-            message: 'Company details updated.',
-            company,
-            success: true,
-        })
-
-    } catch(err) {
-        console.log(err);
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found.",
+        success: false,
+      });
     }
-}
+    return res.status(200).json({
+      message: "Company details updated.",
+      company,
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { registerCompany, getCompany, getCompanyById, updateCompany };
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const dotenv  = require('dotenv')
 const connectDB = require('./utils/db')
 const userRoute = require('./routes/user-route')
+const companyRoute = require('./routes/company-route')
 
 dotenv.config({})
 
@@ -25,6 +26,7 @@ const PORT = process.env.PORT || 3000;
 
 //api's
 app.use('/api/v1/user', userRoute);
+app.use('/api/v1/company', companyRoute);
 
 // 'http://localhost:8000/api/v1/user/register'
 
