b850e73ed1ecc0948139f6e9cd03097af2c5e82b
i_monibKS <singhamonib1327@gmail.com>
2024-09-05 14:28:43 +0530

    modified job-controller logic

--- backend/controllers/job-controller.js
+++ backend/controllers/job-controller.js
@@ -1,6 +1,7 @@
 
 const Job = require('../models/job-model')
 
+// ADMIN 
 const postJob = async (req, res) => {
     try {
         const {title, description, requirements, salary, location, jobType, experience, position, companyId} = req.body;
@@ -34,6 +35,7 @@ const postJob = async (req, res) => {
     }
 }
 
+//Students
 const getAllJobs = async(req, res) => {
     try {
         //filtering using keywords
@@ -63,4 +65,45 @@ const getAllJobs = async(req, res) => {
     }
 }
 
-module.exports = {postJob};
+// Students
+const getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId);
+        if(!job){
+            return res.status(404).json({
+                message: 'Job not found',
+                success: false,
+            })
+        }
+        return res.status(200).json({
+            job,
+            success: true,
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+// ADMIN 
+const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({created_by: adminId});
+        if(!jobs) {
+            return res.status(404).json({
+                message: 'Jobs not found.',
+                success: false,
+            })
+        }
+        return res.status(200).json({
+            jobs,
+            success: true,
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+module.exports = {postJob, getAllJobs, getJobById};
